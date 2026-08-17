c455b4cb302ac98b9dea0f536bd1d5c3ae8033e7
olligator2000 <oleg_luki@mail.ru>
2026-08-17 16:08:33 +0300

    Fix flake8 errors: remove unused imports, fix blank lines2

--- gallery/tests.py
+++ gallery/tests.py
@@ -4,17 +4,17 @@ from django.urls import reverse
 
 class GalleryTests(TestCase):
     def test_homepage_returns_200(self):
-        """Проверяем, что главная страница работает (код 200)"""
+        """Проверяем, что главная страница открывается (код 200)"""
         response = self.client.get(reverse('index'))
         self.assertEqual(response.status_code, 200)
 
     def test_homepage_uses_correct_template(self):
-        """Проверяем, что используется шаблон gallery.html"""
+        """Проверяем, что используется шаблон gallery/index.html"""
         response = self.client.get(reverse('index'))
-        self.assertTemplateUsed(response, 'gallery.html')
+        self.assertTemplateUsed(response, 'gallery/index.html')  # ← правильный путь!
 
     def test_homepage_contains_basic_elements(self):
         """Проверяем, что на странице есть ключевые элементы"""
         response = self.client.get(reverse('index'))
-        self.assertContains(response, 'Галерея')  # текст из заголовка
-        self.assertContains(response, 'DEVOPS')  # текст из бегущей строки
+        self.assertContains(response, 'Галерея')
+        self.assertContains(response, 'DEVOPS')
